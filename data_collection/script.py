473ac01e9f8440ab1148f9c5f4a65a4f93334685
sukumargv <sukumar886@gmail.com>
2016-10-10 02:29:50 -0400

    Psuedo code for one extracting data from one file and storing in json.

--- data_collection/script.py
+++ data_collection/script.py
@@ -1,1 +1,17 @@
-script.py
+import bs4 as bs
+
+
+# load in the file
+
+# get the content in the script tag
+
+# use the regular expression to locate the line
+
+# split the line into fragments
+
+
+# check for json file with present date exists.
+
+# store the contents into a json object
+
+# export json object
